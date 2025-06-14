5b020e8cb74294d8a869036cb7886b3fd31253ea
Sorawit <thankzaki@hotmail.com>
2025-06-14 11:35:38 +0700

    Step 4: sperate the Home widget into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// Step 4: sperate the Home widget into its own file
+// import 'screen/home.dart';
+import 'package:learn_flutter_67_2/screen/home.dart';
+
 void main() {
   runApp(
     MaterialApp(
@@ -14,30 +18,4 @@ void main() {
       ),
     ),
   );
-}
-
-// Step 3: container widget
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // return Container(color: Colors.blueAccent, width: 300, height: 300);
-    // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
-    // return Container(
-    //   color: Colors.blueAccent,
-    //   margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
-    // );
-    return Center(
-      child: Container(
-        color: Colors.blueAccent,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        padding: EdgeInsets.all(20),
-        child: Text(
-          "Hello Flutter",
-          style: TextStyle(fontSize: 30, letterSpacing: 3),
-        ),
-      ),
-    );
-  }
 }
