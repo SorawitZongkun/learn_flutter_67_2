fdab9d8c51215c4f62f85f9a633fc481e5c54b60
Sorawit <thankzaki@hotmail.com>
2025-06-14 11:00:48 +0700

    Step 3: container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // Step 1: appBar and body
-  // runApp(const MyApp());
-
-  // const app = MaterialApp(title: "My Title", home: Text("Hello World"));
-  // runApp(app);
-
-  // runApp(
-  //   MaterialApp(
-  //     title: "My Title",
-  //     home: Scaffold(
-  //       appBar: AppBar(
-  //         title: Text("My App"),
-  //         backgroundColor: Colors.pinkAccent,
-  //         centerTitle: true,
-  //       ),
-  //       body: Text("Hello Flutter"),
-  //     ),
-  //   ),
-  // );
-
-  // Step 2: stateless widget
   runApp(
     MaterialApp(
       title: "My Title",
@@ -37,12 +16,28 @@ void main() {
   );
 }
 
-// Step 2: stateless widget
+// Step 3: container widget
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Flutter");
+    // return Container(color: Colors.blueAccent, width: 300, height: 300);
+    // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
+    // return Container(
+    //   color: Colors.blueAccent,
+    //   margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+    // );
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        padding: EdgeInsets.all(20),
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30, letterSpacing: 3),
+        ),
+      ),
+    );
   }
 }
