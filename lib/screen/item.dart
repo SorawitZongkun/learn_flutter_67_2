4b4005a68f57ec859b1c1b36b27360ac13b18696
Sorawit <thankzaki@hotmail.com>
2025-06-21 10:19:22 +0700

    Step 9: listview widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,4 +1,6 @@
 // Step 8: stateful widget
+// Step 9: listview widget
+
 import 'package:flutter/material.dart';
 
 class Item extends StatefulWidget {
@@ -9,41 +11,67 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  int quantity = 10;
+  // int quantity = 10;
 
-  void addQuantity() {
-    setState(() {
-      quantity++;
-    });
-  }
+  // void addQuantity() {
+  //   setState(() {
+  //     quantity++;
+  //   });
+  // }
 
-  void subtractQuantity() {
-    setState(() {
-      quantity = quantity > 0 ? quantity - 1 : 0;
-    });
-  }
+  // void subtractQuantity() {
+  //   setState(() {
+  //     quantity = quantity > 0 ? quantity - 1 : 0;
+  //   });
+  // }
+
+  // Step 9: listview widget
+  List data = ["สมชาย", "สมหญิง", "สมศรี", "สมปอง", "สมจิตร"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Quantity: $quantity", style: TextStyle(fontSize: 24)),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: addQuantity, child: Text("Add")),
-              SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: subtractQuantity,
-                child: Text("Subtract"),
-              ),
-            ],
+    // return Center(
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       Text("Quantity: $quantity", style: TextStyle(fontSize: 24)),
+    //       SizedBox(height: 20),
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           ElevatedButton(onPressed: addQuantity, child: Text("Add")),
+    //           SizedBox(width: 20),
+    //           ElevatedButton(
+    //             onPressed: subtractQuantity,
+    //             child: Text("Subtract"),
+    //           ),
+    //         ],
+    //       ),
+    //     ],
+    //   ),
+    // );
+
+    // Step 9: listview widget
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.orangeAccent,
           ),
-        ],
-      ),
+          margin: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+          padding: EdgeInsets.all(40),
+          child: Text(
+            data[index],
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+      itemCount: data.length,
     );
   }
 }
